cc09c224a1b4a929411ec14aeef91e81bbc6b2d0
zippiy <3387957+zippiy@users.noreply.github.com>
2020-05-16 10:21:13 +0200

    Update scripts.sh

--- scripts.sh
+++ scripts.sh
@@ -1,6 +1,9 @@
 
--- Simple dump
+-- Simple product listing dump
 curl -X GET  -H 'Accept: application/vnd.allegro.public.v1+json' -H "Authorization: Bearer $token" 'https://api.allegro.pl/offers/listing?phrase=majonez&category.id=73973&sort=+price&offset=0&limit=100' | jq '.items.regular | .[] | {id: .id, name: .name, price: .sellingMode.price.amount, seller: .seller.id}'
+
+-- Simple category dump
+curl -X GET -H "Authorization: Bearer $token" -H 'Accept: application/vnd.allegro.public.v1+json' 'https://api.allegro.pl/sale/categories?parent.id=258832' | jq
 
 -- Number of offers
 curl -X GET  -H 'Accept: application/vnd.allegro.public.v1+json' -H "Authorization: Bearer $token" 'https://api.allegro.pl/offers/listing?phrase=majonez&category.id=73973&sort=+price&offset=0&limit=100' | jq '.filters[0].values[0] | .'
