987a0c3421e4581e27cd88ad818385be6f059a32
zippiy <3387957+zippiy@users.noreply.github.com>
2020-05-16 10:19:30 +0200

    Update scripts.sh

--- scripts.sh
+++ scripts.sh
@@ -1,5 +1,6 @@
 
-
+-- Simple export
 curl -X GET  -H 'Accept: application/vnd.allegro.public.v1+json' -H "Authorization: Bearer $token" 'https://api.allegro.pl/offers/listing?phrase=majonez&category.id=73973&sort=+price&offset=0&limit=100' | jq '.items.regular | .[] | {id: .id, name: .name, price: .sellingMode.price.amount, seller: .seller.id}'
 
+-- Number of offers
 curl -X GET  -H 'Accept: application/vnd.allegro.public.v1+json' -H "Authorization: Bearer $token" 'https://api.allegro.pl/offers/listing?phrase=majonez&category.id=73973&sort=+price&offset=0&limit=100' | jq '.filters[0].values[0] | .'
