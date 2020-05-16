f9614db73a2b9f9fb9b55339e68d7cedda374b6c
zippiy <3387957+zippiy@users.noreply.github.com>
2020-05-16 10:11:14 +0200

    Create scripts.sh

---
+++ scripts.sh
@@ -0,0 +1,3 @@
+
+
+curl -X GET  -H 'Accept: application/vnd.allegro.public.v1+json' -H "Authorization: Bearer $token" 'https://api.allegro.pl/offers/listing?phrase=majonez&category.id=73973&sort=+price&offset=0&limit=100' | jq '.items.regular | .[] | {id: .id, name: .name, price: .sellingMode.price.amount, seller: .seller.id}'
